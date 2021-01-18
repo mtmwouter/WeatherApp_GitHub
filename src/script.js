0b3a2c733984e4a789cceca161a193c424f2e133
mtmwouter <74245592+mtmwouter@users.noreply.github.com>
2021-01-18 15:33:49 +0100

    last final touches, adjusted the font color etc.

--- src/script.js
+++ src/script.js
@@ -69,9 +69,10 @@ function displayForecast(response) {
       <img
         src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
         id="weather-forecast-img"
+        class="float-left"
       />
       <div class="weather-forecast-temperature">
-        <strong> ${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(forecast.main.temp_min)}°
+        <strong> ${Math.round(forecast.main.temp_max)}°</strong> | ${Math.round(forecast.main.temp_min)}°
       </div>
     </div>
     `;
